6b3e6229645f3ee8a7e09bcc85a0071917e2c199
tamago0224 <bigtreesasuke200733@gmail.com>
2018-09-17 10:22:56 +0900

    フォームを用いたサンプル

--- src/main/java/com/narberalgamma/com/demo/HelloController.java
+++ src/main/java/com/narberalgamma/com/demo/HelloController.java
@@ -4,19 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/{num}")
-    public ModelAndView index(@PathVariable int num, ModelAndView mav) {
-        int res = 0;
-        for(int i = 0; i <= num; i++) {
-            res += i;
-        }
-
-        mav.addObject("msg", "total: " + res);
+    @RequestMapping(value="/", method= RequestMethod.GET)
+    public ModelAndView index( ModelAndView mav) {
         mav.setViewName("index");
+        mav.addObject("msg", "お名前を送信してください");
+        return mav;
+    }
+
+    @RequestMapping(value="/", method=RequestMethod.POST)
+    public ModelAndView send(@RequestParam("text1")String str, ModelAndView mav) {
+        mav.addObject("msg", "こんにちは" + str + "さん!");
+        mav.addObject("value", str);
+        mav.setViewName("index");
+
         return mav;
     }
 }
