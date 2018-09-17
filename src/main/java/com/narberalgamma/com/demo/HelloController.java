d69e82b555cf651fe5589415f4f35109e90bde88
tamago0224 <bigtreesasuke200733@gmail.com>
2018-09-17 21:53:24 +0900

    redirectの実装

--- src/main/java/com/narberalgamma/com/demo/HelloController.java
+++ src/main/java/com/narberalgamma/com/demo/HelloController.java
@@ -8,21 +8,47 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
 public class HelloController {
     @RequestMapping(value="/", method= RequestMethod.GET)
     public ModelAndView index( ModelAndView mav) {
         mav.setViewName("index");
-        mav.addObject("msg", "お名前を送信してください");
+        mav.addObject("msg", "フォームを送信してください");
         return mav;
     }
 
     @RequestMapping(value="/", method=RequestMethod.POST)
-    public ModelAndView send(@RequestParam("text1")String str, ModelAndView mav) {
-        mav.addObject("msg", "こんにちは" + str + "さん!");
-        mav.addObject("value", str);
+    public ModelAndView send(
+            @RequestParam(value = "check1", required = false)boolean check1,
+            @RequestParam(value="radio1", required = false)String radio1,
+            @RequestParam(value="select1", required = false)String select1,
+            @RequestParam(value="select2", required = false)String[] select2,
+            ModelAndView mav) {
+        String res = "";
+        try {
+            res = "check: " + check1 +
+                    " radio: " + radio1 +
+                    " select: " + select1 +
+                    "\nselect2: ";
+        } catch(NullPointerException e) {}
+
+        try {
+            res += select2[0];
+            for(int i = 0; i < select2.length; i++) {
+                res += "," + select2[i];
+            }
+        } catch(NullPointerException e){
+            res += "null";
+        }
+        mav.addObject("msg", res);
         mav.setViewName("index");
 
         return mav;
+    }
+
+    @RequestMapping("/other")
+    public String other() {
+        return "redirect:/";
     }
 }
