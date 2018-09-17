e9428b6c81e3be6ace273612f4a53b1d40300aef
tamago0224 <bigtreesasuke200733@gmail.com>
2018-09-17 23:13:45 +0900

    オブジェクトのテンプレートへの挿入

--- src/main/java/com/narberalgamma/com/demo/HelloController.java
+++ src/main/java/com/narberalgamma/com/demo/HelloController.java
@@ -1,6 +1,10 @@
 package com.narberalgamma.com.demo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,44 +15,22 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
-    @RequestMapping(value="/", method= RequestMethod.GET)
-    public ModelAndView index( ModelAndView mav) {
+    @RequestMapping("/")
+    public ModelAndView index(ModelAndView mav) {
         mav.setViewName("index");
-        mav.addObject("msg", "フォームを送信してください");
+        mav.addObject("msg", "current data.");
+        DataObject obj = new DataObject(123, "hanako", "hanako@flower");
+        mav.addObject("object", obj);
         return mav;
     }
+}
 
-    @RequestMapping(value="/", method=RequestMethod.POST)
-    public ModelAndView send(
-            @RequestParam(value = "check1", required = false)boolean check1,
-            @RequestParam(value="radio1", required = false)String radio1,
-            @RequestParam(value="select1", required = false)String select1,
-            @RequestParam(value="select2", required = false)String[] select2,
-            ModelAndView mav) {
-        String res = "";
-        try {
-            res = "check: " + check1 +
-                    " radio: " + radio1 +
-                    " select: " + select1 +
-                    "\nselect2: ";
-        } catch(NullPointerException e) {}
 
-        try {
-            res += select2[0];
-            for(int i = 0; i < select2.length; i++) {
-                res += "," + select2[i];
-            }
-        } catch(NullPointerException e){
-            res += "null";
-        }
-        mav.addObject("msg", res);
-        mav.setViewName("index");
-
-        return mav;
-    }
-
-    @RequestMapping("/other")
-    public String other() {
-        return "redirect:/";
-    }
+@Getter
+@Setter
+@AllArgsConstructor
+class DataObject {
+    private int id;
+    private String name;
+    private String value;
 }
