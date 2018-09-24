214a57d0584a22611be695c5813796fd34444b09
tamago0224 <bigtreesasuke200733@gmail.com>
2018-09-24 16:31:13 +0900

    トランザクションと用いたデータ処理

--- src/main/java/com/narberalgamma/com/demo/HelloController.java
+++ src/main/java/com/narberalgamma/com/demo/HelloController.java
@@ -1,22 +1,47 @@
 package com.narberalgamma.com.demo;
 
 import com.narberalgamma.com.demo.repositories.MyDataRepository;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.PostConstruct;
+import java.util.Optional;
 
 
 @Controller
 public class HelloController {
     @Autowired
     MyDataRepository repository;
+
+    @PostConstruct
+    public void init() {
+        //１つ目のダミーデータ
+        MyData d1 = new MyData();
+        d1.setName("tuyano");
+        d1.setAge(123);
+        d1.setMail("syada@tuyano.com");
+        d1.setMemo("This is first dummy data.");
+        repository.saveAndFlush(d1);
+
+        //２つ目のダミーデータ
+        MyData d2 = new MyData();
+        d2.setName("hanako");
+        d2.setAge(23);
+        d2.setMail("hanako@flower");
+        d2.setMemo("this is second dummy data.");
+        repository.saveAndFlush(d2);
+
+        //３つ目のダミーデータ
+        MyData d3 = new MyData();
+        d3.setName("sachiko");
+        d3.setAge(43);
+        d3.setMail("sachiko@happy");
+        d3.setMemo("This is third dummy data.");
+        repository.saveAndFlush(d3);
+    }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView index(
@@ -37,4 +62,23 @@ public class HelloController {
         repository.saveAndFlush(mydata);
         return new ModelAndView("redirect:/");
     }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public ModelAndView edit(@ModelAttribute MyData mydata,
+                             @PathVariable int id, ModelAndView mav) {
+        mav.setViewName("edit");
+        mav.addObject("title", "edit mydata.");
+        Optional<MyData> data = repository.findById((long)id);
+        mav.addObject("formModel", data.get());
+        return mav;
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    @Transactional(readOnly = false)
+    public ModelAndView edit(@ModelAttribute MyData mydata,
+                             ModelAndView mav) {
+        repository.saveAndFlush(mydata);
+        return new ModelAndView("redirect:/");
+    }
+
 }
